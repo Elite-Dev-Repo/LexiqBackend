05c7cfde3f956cfacf132e30a8416ab8896f8819
Elite Dev <oyenekanemmanuel02@gmail.com>
2026-07-28 01:02:39 +0100

    feat: configure ASGI application with Channels routing for WebSocket support

--- LexiqBackend/asgi.py
+++ LexiqBackend/asgi.py
@@ -6,15 +6,16 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
-
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LexiqBackend.settings')
+
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from Rooms.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LexiqBackend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
