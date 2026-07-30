70f6b5e2029675a7b95d937f43e61263fb569e32
Elite Dev <oyenekanemmanuel02@gmail.com>
2026-07-30 23:29:58 +0100

    feat: implement WebSocket chat functionality with database models and routing

---
+++ MainChat/consumers.py
@@ -0,0 +1,48 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+from channels.db import database_sync_to_async
+from .models import Message
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = f'chat_global'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
+
+        # Save message to database
+        Message.objects.create(user=username, message=message)
+
+        # Broadcast message to room
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+                'username': username,
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'username': username,
+        }))
